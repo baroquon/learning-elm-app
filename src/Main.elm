621ce1724ac8b9513d41c0bc877bdd7908b477a7
Brandon Blaylock <brandon.blaylock@gmail.com>
2019-10-25 14:12:02 -0400

    initial commit

---
+++ src/Main.elm
@@ -0,0 +1,103 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, text, div, h1, img, input, br)
+import Html.Attributes exposing (src, class, value)
+import Html.Events exposing (onInput)
+
+
+---- MODEL ----
+
+type alias Team = 
+    {
+        id: Int
+    ,   name: String
+    ,   city: String
+    }
+
+type alias Model =
+    {
+        teams : List Team
+    }
+
+team1 = 
+  {
+    id=1
+  , name = "Cubs"
+  , city = "Chicago"
+  }
+team2 = 
+  {
+    id=2
+  , name = "Cardinals"
+  , city = "St. Louis"
+  }
+team3 = 
+  {
+    id=3
+  , name = "Marlins"
+  , city = "Miami"
+  }
+team4 = 
+  {
+    id=4
+  , name = "Braves"
+  , city = "Atlanta"
+  }
+init : ( Model, Cmd Msg )
+init =
+    ( { teams=[team1, team2, team3, team4] }, Cmd.none )
+
+
+
+---- UPDATE ----
+type Msg
+  = Change Int String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Change id newContent ->
+      let
+          updateEntry t =
+              if t.id == id then
+                  { t | name = newContent }
+              else
+                  t
+      in
+      ( { model | teams = List.map updateEntry model.teams }
+      , Cmd.none
+      )
+
+---- VIEW ----
+mapFunc : Team -> Html Msg
+mapFunc passedInTeam =
+  div []
+    [  text passedInTeam.name
+    , br [] []
+    , input [ value passedInTeam.name, onInput (Change passedInTeam.id)] []
+    ]
+
+view : Model -> Html Msg
+view model =
+    div 
+      [ class "hello-container" ]
+        [ img [ src "/logo.svg" ] []
+        , h1 [] [ text "List of Teams" ]
+        , div [] (List.map mapFunc model.teams)
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
